9d83ca1fa75b3266313158a9a3b546c5c6c885be
Thomas Wilburn <news@thomaswilburn.net>
2016-07-15 12:21:30 -0700

    Click for fullscreen behavior

---
+++ readme.rst
@@ -0,0 +1,4 @@
+UOS Offline Player
+==================
+
+You will need the video files for this, we're not checking them in.
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var videoTemplate = function(data) {
   <div class="video-container">
     <h2 class="title">${data.title}</h2>
     <div class="aspect">
-      <video>
+      <video preload="metadata">
         <source src="${data.source}"></source>
         <track src="${data.caption}"></track>
       </video>
@@ -14,6 +14,41 @@ var videoTemplate = function(data) {
 
 var words = document.querySelector(".word.videos");
 
-for (var i = 0; i < 20; i++) {
-  words.innerHTML += videoTemplate({});
+for (var i = 0; i < 5; i++) {
+  words.innerHTML += videoTemplate({ source: "./small.mp4" });
 }
+
+var $ = s => [].slice.call(document.querySelectorAll(s));
+var fullscreen = document.body.webkitRequestFullscreen ? "webkitRequestFullscreen" : "requestFullscreen";
+
+var playing = null;
+var onClick = function() {
+  this.play();
+  this[fullscreen]();
+  this.setAttribute("controls", "");
+  playing = this;
+};
+
+var onStop = function() {
+  console.log("stop", this);
+  this.removeAttribute("controls");
+  this.currentTime = 0;
+}
+
+var onExit = function() {
+  if (!playing || document.fullscreenElement || document.webkitFullscreenElement) return;
+  playing.pause();
+  playing.currentTime = 0;
+  onStop.call(playing);
+  playing = null;
+};
+
+var videos = $("video")
+
+videos.forEach(function(element) {
+  element.addEventListener("click", onClick);
+  element.addEventListener("pause", onStop);
+});
+
+document.addEventListener("fullscreenchange", onExit);
+document.addEventListener("webkitfullscreenchange", onExit);
