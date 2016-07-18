9f40ead0c11097055a305822126fac91eaf7d93a
Audrey Carlsen <acarlsen@seattletimes.com>
2016-07-18 16:58:21 -0700

    adds header, repositioned titles, etc

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var videoTemplate = function(data) {
   return `
   <div class="video-container">
-    <h2 class="title">${data.title || "..."}</h2>
+    <h3 class="title">${data.title || "..."}</h3>
     <div class="aspect">
-      <video preload="metadata" poster="">
+      <video class="video" preload="metadata" poster="">
         <source src="${data.video}"></source>
         <track default kind="captions" label="CC" src="${data.caption}"></track>
       </video>
@@ -13,18 +13,39 @@ var videoTemplate = function(data) {
 };
 
 var words = {
-  "Ally": { video: "20160614_Ally.mp4", caption: "ALLY-captions.vtt" },
-  "All lives matter": { video: "20160614_AllLivesMatter.mp4", caption: "ALLLIVESMATTER-captions.vtt" },
-  "Colorblindness": { video: "20160614_Colorblindness_CC.mp4", caption: "COLORBLIND-captions.vtt" },
-  "Diversity": { video: "20160613_diversity.mp4", caption: "DIVERSITY-captions.vtt" },
-  "Microaggression": { video: "20160613_microaggression.mp4", caption: "MICROAGGRESSION-captions.vtt" },
+  "Institutional racism": { video: "20160616_Racism.mp4", caption: "RACISM-captions.vtt" },
   "Person of color": { video: "20160614_Person of Color_CC.mp4", caption: "POC-captions.vtt" },
-  "Politically correct": { video: "20160616_Politically_Correct.mp4", caption: "PC-captions.vtt" },
-  "Racism": { video: "20160616_Racism.mp4", caption: "RACISM-captions.vtt" },
   "Racist": { video: "20160613_racist.mp4", caption: "RACIST-captions.vtt" },
+  "Ally": { video: "20160614_Ally.mp4", caption: "ALLY-captions.vtt" },
+  "Microaggression": { video: "20160613_microaggression.mp4", caption: "MICROAGGRESSION-captions.vtt" },
+  "All lives matter": { video: "20160614_AllLivesMatter.mp4", caption: "ALLLIVESMATTER-captions.vtt" },
+  "Politically correct": { video: "20160616_Politically_Correct.mp4", caption: "PC-captions.vtt" },
+  "Colorblindness": { video: "20160614_Colorblindness_CC.mp4", caption: "COLORBLIND-captions.vtt" },
   "Safe space": { video: "20160614_safe space.mp4", caption: "SAFESPACE-captions.vtt" },
+  "Diversity": { video: "20160613_diversity.mp4", caption: "DIVERSITY-captions.vtt" },
   "White privilege": { video: "20160613_white privilege.mp4", caption: "WHITEPRIVILEGE-captions.vtt" },
   "White fragility": { video: "20160614_White Fragility_CC.mp4", caption: "FRAGILITY-captions.vtt" }
+};
+
+var people = {
+  "Duck Bae": { video: "20160616_Racism.mp4", caption: "RACISM-captions.vtt" },
+  "Autumn Bennett": { video: "20160614_Person of Color_CC.mp4", caption: "POC-captions.vtt" },
+  "Keli Carender": { video: "20160613_racist.mp4", caption: "RACIST-captions.vtt" },
+  "Louis Chude-Sokei": { video: "20160614_Ally.mp4", caption: "ALLY-captions.vtt" },
+  "Jerrell Davis": { video: "20160613_microaggression.mp4", caption: "MICROAGGRESSION-captions.vtt" },
+  "Rachael DeCruz": { video: "20160614_AllLivesMatter.mp4", caption: "ALLLIVESMATTER-captions.vtt" },
+  "Michael Dixon": { video: "20160616_Politically_Correct.mp4", caption: "PC-captions.vtt" },
+  "Leija Farr": { video: "20160614_Colorblindness_CC.mp4", caption: "COLORBLIND-captions.vtt" },
+  "M. Lorena Gonzalez": { video: "20160614_safe space.mp4", caption: "SAFESPACE-captions.vtt" },
+  "Darrell Hillaire": { video: "20160613_diversity.mp4", caption: "DIVERSITY-captions.vtt" },
+  "Varisha Khan": { video: "20160613_white privilege.mp4", caption: "WHITEPRIVILEGE-captions.vtt" },
+  "Lucas Nydam": { video: "20160614_White Fragility_CC.mp4", caption: "FRAGILITY-captions.vtt" },
+  "Mark Olsen": { video: "20160614_Colorblindness_CC.mp4", caption: "COLORBLIND-captions.vtt" },
+  "Marci Owens": { video: "20160614_safe space.mp4", caption: "SAFESPACE-captions.vtt" },
+  "Greg Rickel": { video: "20160613_diversity.mp4", caption: "DIVERSITY-captions.vtt" },
+  "Sean Riley": { video: "20160613_white privilege.mp4", caption: "WHITEPRIVILEGE-captions.vtt" },
+  "Cynthia Tee": { video: "20160614_White Fragility_CC.mp4", caption: "FRAGILITY-captions.vtt" },
+  "Tariqa Waters": { video: "20160614_Colorblindness_CC.mp4", caption: "COLORBLIND-captions.vtt" }
 };
 
 var wordContainer = document.querySelector(".word.videos");
@@ -43,12 +64,12 @@ var fullscreenElement = "webkitFullscreenElement" in document ? "webkitFullscree
 
 var playing = null;
 var onClick = function() {
-  if (document[fullscreenElement] == this) return;
-  console.log(this);
-  this[fullscreen]();
-  this.play();
-  this.setAttribute("controls", "");
-  playing = this;
+  var video = this.getElementsByClassName('video')[0];
+  if (document[fullscreenElement] == video) return;
+  video[fullscreen]();
+  video.play();
+  video.setAttribute("controls", "");
+  playing = video;
 };
 
 var onExit = function() {
@@ -61,9 +82,7 @@ var onExit = function() {
   playing = null;
 };
 
-var videos = $("video")
-
-videos.forEach(function(element) {
+$(".video-container").forEach(function(element) {
   element.addEventListener("click", onClick);
 });
 
