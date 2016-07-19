401bc1bcd3e8eae392d721b61454335e6d3df1f2
Audrey Carlsen <acarlsen@seattletimes.com>
2016-07-19 11:03:57 -0700

    swaps videos for word tiles and headshots

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-var videoTemplate = function(data) {
+var wordTemplate = function(data) {
   return `
   <div class="video-container">
-    <h3 class="title">${data.title || "..."}</h3>
-    <div class="aspect">
+    <div class="title">${data.title || "..."}</div>
       <video class="video" preload="metadata" poster="">
+      <div class="close">CLOSE</div>
         <source src="${data.video}"></source>
         <track default kind="captions" label="CC" src="${data.caption}"></track>
       </video>
-    </div>
+  </div>
+  `;
+};
+
+var bioTemplate = function(data) {
+  return `
+  <div class="video-container">
+
+      <img src="${data.still}">
+      <video class="video" preload="metadata" poster="">
+        <source src="${data.video}"></source>
+        // <track default kind="captions" label="CC" src="${data.caption}"></track>
+      </video>
+
+    <h3 class="name">${data.name || "..."}</h3>
   </div>
   `;
 };
@@ -28,24 +42,24 @@ var words = {
 };
 
 var people = {
-  "Duck Bae": { video: "20160616_Racism.mp4", caption: "RACISM-captions.vtt" },
-  "Autumn Bennett": { video: "20160614_Person of Color_CC.mp4", caption: "POC-captions.vtt" },
-  "Keli Carender": { video: "20160613_racist.mp4", caption: "RACIST-captions.vtt" },
-  "Louis Chude-Sokei": { video: "20160614_Ally.mp4", caption: "ALLY-captions.vtt" },
-  "Jerrell Davis": { video: "20160613_microaggression.mp4", caption: "MICROAGGRESSION-captions.vtt" },
-  "Rachael DeCruz": { video: "20160614_AllLivesMatter.mp4", caption: "ALLLIVESMATTER-captions.vtt" },
-  "Michael Dixon": { video: "20160616_Politically_Correct.mp4", caption: "PC-captions.vtt" },
-  "Leija Farr": { video: "20160614_Colorblindness_CC.mp4", caption: "COLORBLIND-captions.vtt" },
-  "M. Lorena Gonzalez": { video: "20160614_safe space.mp4", caption: "SAFESPACE-captions.vtt" },
-  "Darrell Hillaire": { video: "20160613_diversity.mp4", caption: "DIVERSITY-captions.vtt" },
-  "Varisha Khan": { video: "20160613_white privilege.mp4", caption: "WHITEPRIVILEGE-captions.vtt" },
-  "Lucas Nydam": { video: "20160614_White Fragility_CC.mp4", caption: "FRAGILITY-captions.vtt" },
-  "Mark Olsen": { video: "20160614_Colorblindness_CC.mp4", caption: "COLORBLIND-captions.vtt" },
-  "Marci Owens": { video: "20160614_safe space.mp4", caption: "SAFESPACE-captions.vtt" },
-  "Greg Rickel": { video: "20160613_diversity.mp4", caption: "DIVERSITY-captions.vtt" },
-  "Sean Riley": { video: "20160613_white privilege.mp4", caption: "WHITEPRIVILEGE-captions.vtt" },
-  "Cynthia Tee": { video: "20160614_White Fragility_CC.mp4", caption: "FRAGILITY-captions.vtt" },
-  "Tariqa Waters": { video: "20160614_Colorblindness_CC.mp4", caption: "COLORBLIND-captions.vtt" }
+  "Duck Bae": { video: "duck_bio.mp4", caption: "RACISM-captions.vtt", still: "duck_still" },
+  "Autumn Bennett": { video: "Autumn_bio.mp4", caption: "POC-captions.vtt", still: "autumn_still" },
+  "Keli Carender": { video: "keli_bio.mp4", caption: "RACIST-captions.vtt", still: "keli_still" },
+  "Louis Chude-Sokei": { video: "Louis_bio.mp4", caption: "ALLY-captions.vtt", still: "louis_still" },
+  "Jerrell Davis": { video: "Jerrell_bio.mp4", caption: "MICROAGGRESSION-captions.vtt", still: "jerrell_still" },
+  "Rachael DeCruz": { video: "rachael_bio.mp4", caption: "ALLLIVESMATTER-captions.vtt", still: "rachael_still" },
+  "Michael Dixon": { video: "michael_bio.mp4", caption: "PC-captions.vtt", still: "michael_still" },
+  "Leija Farr": { video: "leija_bio.mp4", caption: "COLORBLIND-captions.vtt", still: "leija_still" },
+  "M. Lorena Gonzalez": { video: "lorena_bio.mp4", caption: "SAFESPACE-captions.vtt", still: "lorena_still" },
+  "Darrell Hillaire": { video: "darrell_bio.mp4", caption: "DIVERSITY-captions.vtt", still: "darrell_still" },
+  "Varisha Khan": { video: "varisha_bio.mp4", caption: "WHITEPRIVILEGE-captions.vtt", still: "varisha_still" },
+  "Lucas Nydam": { video: "lucas_bio.mp4", caption: "FRAGILITY-captions.vtt", still: "lucas_still" },
+  "Mark Olsen": { video: "mark_bio.mp4", caption: "COLORBLIND-captions.vtt", still: "mark_still" },
+  "Marci Owens": { video: "marci_bio.mp4", caption: "SAFESPACE-captions.vtt", still: "marci_still" },
+  "Greg Rickel": { video: "greg_bio.mp4", caption: "DIVERSITY-captions.vtt", still: "greg_still" },
+  "Sean Riley": { video: "sean_bio.mp4", caption: "WHITEPRIVILEGE-captions.vtt", still: "sean_still" },
+  "Cynthia Tee": { video: "cynthia_bio.mp4", caption: "FRAGILITY-captions.vtt", still: "cynthia_still" },
+  "Tariqa Waters": { video: "20160613_tariqa_bio.mp4", caption: "COLORBLIND-captions.vtt", still: "tariqa_still" }
 };
 
 var wordContainer = document.querySelector(".word.videos");
@@ -54,7 +68,17 @@ for (var title in words) {
   var word = words[title];
   var video = "./videos/words/" + word.video;
   var caption = "./videos/words/" + word.caption;
-  wordContainer.innerHTML += videoTemplate({ title, video, caption });
+  wordContainer.innerHTML += wordTemplate({ title, video, caption });
+}
+
+var bioContainer = document.querySelector(".bio.videos");
+
+for (var name in people) {
+  var person = people[name];
+  var video = "./videos/bios/" + person.video;
+  var caption = "";
+  var still = "./stills/" + person.still + ".jpg";
+  bioContainer.innerHTML += bioTemplate({ name, video, still });
 }
 
 var $ = s => [].slice.call(document.querySelectorAll(s));
@@ -73,7 +97,6 @@ var onClick = function() {
 };
 
 var onExit = function() {
-  console.log("exit", document[fullscreenElement], playing);
   if (document[fullscreenElement]) return;
   playing.pause();
   playing.currentTime = 0;
